14bd76b011a412f3f790c98f91eb54d7b7109ed3
dymani <auyf.g118@gmail.com>
2015-09-24 22:02:52 +0800

    Scripting system

---
+++ assets/data/gameStateInit.lua
@@ -0,0 +1,7 @@
+gameStateInit = {
+    init=function(gameStateInit)
+        gameStateInit:addEntity("test1", 306, 238, 0)
+        gameStateInit:addEntity("test2", 356, 238, 1)
+        gameStateInit:addEntity("test3", 406, 238, 2)
+    end
+}
